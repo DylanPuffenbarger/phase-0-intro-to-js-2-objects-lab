648b3b6fdd188aff29361b5089e5850a70f63e0c
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-07-26 20:04:12 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name : 'Dylan',
+    streetAddress : '46 Beacon'
+};
+
+function updateEmployeeWithKeyAndValue(employeeObject, key, value){
+    const employee_copy = {...employeeObject};
+    employee_copy[key] = value;
+    return employee_copy
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value){
+    employeeObject[key] = value;
+    return employeeObject;
+}
+
+function deleteFromEmployeeByKey(employeeObject, key){
+    const employee_copy = {...employeeObject};
+    delete employee_copy[key];
+    return employee_copy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key){
+    delete employeeObject[key];
+    return employeeObject;
+}
